572a2c3a7bb914cdc6e99d2e9b7c7ef2b3231d28
SamuelMartins03 <98755249+SamuelMartins03@users.noreply.github.com>
2022-03-02 11:44:08 +0000

    organize assets

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 50f;
+    [SerializeField] GameObject viewDamageAmount;
 
     public void TakeDamage(float firePower)
     {
         health -= firePower;
+
+        GameObject DamageAmountInstantiated = Instantiate(viewDamageAmount, gameObject.transform);
+        Destroy(DamageAmountInstantiated, 1f);
 
         if (health <= 0)
         {
